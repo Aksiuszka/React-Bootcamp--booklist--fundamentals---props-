593313c581dc16c664b1d807b42f6431f9f6eb7b
Aksiuszka <aakawecka89@gmail.com>
2021-08-11 08:53:57 +0200

    React Bootcamp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books =[
+{
+  img:'https://images-na.ssl-images-amazon.com/images/I/51Tg0DASz8L.jpg',
+  title:'Folwark Zwierzęcy',
+  author:'George Orwell'
+},
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+{
+  img:'https://images-na.ssl-images-amazon.com/images/I/31aaCfERsSL._SX280_BO1,204,203,200_.jpg',
+  title:'Marsjanin',
+  author:'Andy Weir'
+},
+{
+  img:'https://images-na.ssl-images-amazon.com/images/I/419kvKhJbRL._SX358_BO1,204,203,200_.jpg',
+  title:'The Resurrectionist: The Lost Work and Writings of Dr. Spencer Black',
+  author:'E. B. Hudspeth'
+},
+{
+  img:'https://images-na.ssl-images-amazon.com/images/I/51nOdhcOEKL._SX333_BO1,204,203,200_.jpg',
+  title:'Metro 2035',
+  author:'Dmitry Glukhovsky'
+},
+{
+  img:'https://images-na.ssl-images-amazon.com/images/I/51Dmd8L6ykL._SX325_BO1,204,203,200_.jpg',
+  title:'Zgromadzenie cieni',
+  author:'V. E. Schwab '
+},
+{
+  img:'https://images-na.ssl-images-amazon.com/images/I/51rGHSSyvxL._SX301_BO1,204,203,200_.jpg',
+  title:'Salems Lot',
+  author:'Stephen King'
+},
+
+{
+  img:'https://images-na.ssl-images-amazon.com/images/I/717hRhPPQWL.jpg',
+  title:'Smętarz dla zwierząt',
+  author:'Stephen King'
+},
+];
+
+const Booklist = () => {
+  return(
+    <section>
+      <div className="entry">
+        
+       {books.map((book)=>{
+         const {img,author, title} = book;
+         return <div class="bookie">
+           <h3>{title}</h3>
+           <h6>{author}</h6>
+           <img src={img} alt="xxx"/>
+         </div>
+       })  }
+         
+      </div>
+    </section>
+  );
+}
+
+
+
+const Book = ({img, title, author, children}) => {
+  return (
+    <div className="bookie">
+    <img className="book" src={img} alt="xxx"/>
+    <h2 className="title">{title}</h2>
+    <h4 className="author">{author}</h4>
+    
+    </div>
+  )
+};
+
+
+ReactDom.render(<Booklist/>,document.getElementById('root')); 
